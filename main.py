859999c56f0da079c1390b60f19c92bccfcd7e67
louisabraham <louis.abraham@yahoo.fr>
2017-01-27 09:59:47 +0100

    add comments

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ from network.proxychains import launchDofus
 # to interrupt : httpd.shutdown()
 httpd = startProxyServer()
 
+# you can launch several instances
+# of dofus with the same httpd
+# the bot will be launched after the connexion
 dofus = launchDofus()
