e8bd709fabbf4db01ab13c1d06333abcc2e53027
louisabraham <louis.abraham@yahoo.fr>
2018-04-15 21:40:00 +0200

    correct bug in readBooleans add write methods

--- labot/protocol.py
+++ labot/protocol.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 from .protocolBuilder import types, msg_from_id, types_from_id, primitives
 from .data import Data
 
@@ -10,13 +12,14 @@ primitives = {
 }
 
 
-def readBooleans(type, data):
-    ans = dict(__type__=type['name'])
-    boolVars = type['boolVars']
+def readBooleans(boolVars, data):
+    ans = {}
     bvars = iter(boolVars)
     for _ in range(0, len(boolVars), 8):
         bits = format(data.readByte(), '08b')[::-1]
-        for var, val in zip(bvars, bits):
+        for val, var in zip(bits, bvars):
+            # be careful, you have to
+            # put bits first in zip
             ans[var['name']] = val == '1'
     return ans
 
@@ -44,10 +47,56 @@ def read(type, data):
     parent = type['parent']
     if parent is not None:
         ans.update(read(parent, data))
-    ans.update(readBooleans(type, data))
+        ans['__type__'] = type['name']
+    ans.update(readBooleans(type['boolVars'], data))
     for var in type['vars']:
         if var['length'] is not None:
             ans[var['name']] = readVec(var, data)
         else:
             ans[var['name']] = read(var['type'], data)
     return ans
+
+
+def writeBooleans(boolVars, el, data):
+    bits = []
+    for var in boolVars:
+        bits.append(el[var['name']])
+        if len(bits) == 8:
+            data.writeByte(reduce(lambda a, b: 2 * a + b, bits[::-1]))
+            bits = []
+    if bits:
+        data.writeByte(reduce(lambda a, b: 2 * a + b, bits[::-1]))
+
+
+def writeVec(var, el, data):
+    assert var['length'] is not None
+    n = len(el)
+    if isinstance(var['length'], int):
+        assert n == var['length']
+    else:
+        write(var['length'], n, data)
+    for it in el:
+        write(var['type'], it, data)
+
+
+def write(type, json, data=None):
+    if data is None:
+        data = Data()
+    if type is False:
+        type = types[json['__type__']]
+        data.writeUnsignedShort(type['protocolId'])
+    elif isinstance(type, str):
+        if type in primitives:
+            primitives[type][1](data, json)
+            return data
+        type = types[type]
+    parent = type['parent']
+    if parent is not None:
+        write(parent, json, data)
+    writeBooleans(type['boolVars'], json, data)
+    for var in type['vars']:
+        if var['length'] is not None:
+            writeVec(var, json[var['name']], data)
+        else:
+            write(var['type'], json[var['name']], data)
+    return data
