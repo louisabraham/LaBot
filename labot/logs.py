09685e8adb796c564313365f6a5089db8e1b5bd7
Luca Dorigo <dorigoluca@gmail.com>
2018-05-15 13:20:38 +0200

    Added configuration for logger

---
+++ labot/logs.py
@@ -0,0 +1,55 @@
+import logging
+
+# Colored logs mainly a courtesy of 
+# https://stackoverflow.com/questions/384076/how-can-i-color-python-logging-output
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+
+# The background is set with 40 plus the number of the color, and the foreground with 30
+# These are the sequences need to get colored ouput
+RESET_SEQ = "\033[0m"
+COLOR_SEQ = "\033[1;%dm"
+BOLD_SEQ = "\033[1m"
+
+
+def formatter_message(message, use_color=True):
+    if use_color:
+        message = message.replace(
+            "$RESET", RESET_SEQ).replace("$BOLD", BOLD_SEQ)
+    else:
+        message = message.replace("$RESET", "").replace("$BOLD", "")
+    return message
+
+
+COLORS = {
+    'WARNING': YELLOW,
+    'INFO': WHITE,
+    'DEBUG': BLUE,
+    'CRITICAL': YELLOW,
+    'ERROR': RED
+}
+
+
+class ColoredFormatter(logging.Formatter):
+    def __init__(self, msg, use_color=True):
+        logging.Formatter.__init__(self, msg, "%H:%M:%S")
+        self.use_color = use_color
+
+    def format(self, record):
+        levelname = record.levelname
+        if self.use_color and levelname in COLORS:
+            levelname_color = COLOR_SEQ % (
+                30 + COLORS[levelname]) + levelname + RESET_SEQ
+            record.levelname = levelname_color
+        return logging.Formatter.format(self, record)
+
+
+FORMAT = '%(asctime)s.%(msecs)03d %(threadName)s: function "%(funcName)s" of module "%(module)s"  \n %(levelname)s: %(message)s'
+COLOR_FORMAT = formatter_message(FORMAT, True)
+
+
+color_formatter = ColoredFormatter(FORMAT)
+logger=logging.getLogger()
+handler=logging.StreamHandler()
+handler.setFormatter(color_formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
