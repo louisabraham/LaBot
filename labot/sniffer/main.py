9bde41eda15ecbbd08c0ab239465e72f96791e67
Luca Dorigo <dorigoluca@gmail.com>
2018-05-17 18:56:40 +0200

    Added command-line toggle for logger level

--- labot/sniffer/main.py
+++ labot/sniffer/main.py
@@ -12,12 +12,20 @@ def main(capture_file=None):
 
 if __name__ == "__main__":
 
-    logger.debug("Starting sniffer as __main__")
     parser = argparse.ArgumentParser(
         description='Start the sniffer either from a file or from live capture.')
     parser.add_argument('--capture', '-c', metavar='PATH', type=str,
                         help='Path to capture file')
-    args = parser.parse_args()
+    parser.add_argument('--debug', '-d', action='store_true',
+                        help='show logger debug messages')
+
+    args = parser.parse_args()    
+
+    if(args.debug):
+        logger.setLevel("DEBUG")
+    else:
+        logger.setLevel("INFO")
+
     if args.capture:
         logger.debug("Starting sniffer with capture file")
         main(args.capture)
