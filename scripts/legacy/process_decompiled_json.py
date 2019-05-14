86d7a88f033f0d609b9562e0f3ae993cb4336cbb
louisabraham <louis.abraham@yahoo.fr>
2019-05-14 16:59:50 +0200

    move scripts to scripts/

---
+++ scripts/legacy/process_decompiled_json.py
@@ -0,0 +1,62 @@
+import logging
+import json
+import pickle
+import os
+
+logger = logging.getLogger()
+
+
+def make_i18n_pickle():
+    path = os.getcwd() + "/sources/data/i18n_fr.json"
+    logger.debug(path)
+
+    try:
+        if not os.path.exists("./sources/pickles"):
+            os.makedirs("./sources/pickles")
+
+        f = open(path, encoding="utf-8", mode="r")
+        i18n = json.load(f)
+        f.close()
+        f = open("./sources/pickles/I18nPickle", "wb+")
+        i18n_dict = i18n["texts"]
+        pickle.dump(i18n_dict, f)
+        f.close()
+        logger.debug("Made pickle with i18n information from %s", path)
+        return i18n_dict
+    except FileNotFoundError:
+        logger.error(
+            "Coudn't find json file. Please make sure to run decompile.sh before this script."
+        )
+
+
+def read_objects():
+    path = "./sources/data/Items.json"
+    logger.debug("Making pickle with item information from %s", path)
+
+    try:
+        f = open(path, "r")
+        items = json.load(f)
+        item_dict = {}
+        for item in items:
+            item_dict[item["id"]] = item
+        f.close()
+
+        if not os.path.exists("./sources/pickles"):
+            os.makedirs("./sources/pickles")
+
+        f = open("./sources/pickles/ItemsPickle", "wb+")
+        pickle.dump(item_dict, f)
+        f.close()
+        logger.debug(
+            "Made pickle with item information at %s", "./sources/pickles/ItemsPickle"
+        )
+        return item_dict
+    except FileNotFoundError:
+        logger.error(
+            "Coudn't find json file. Please make sure to run decompile.sh before this script."
+        )
+
+
+if __name__ == "__main__":
+    make_i18n_pickle()
+    read_objects()
