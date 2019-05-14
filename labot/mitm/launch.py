3b4b8bc3225437b21a58200095f1f41131261874
louisabraham <louis.abraham@yahoo.fr>
2019-05-14 12:36:56 +0200

    new callback to retrieve bridges

--- labot/mitm/launch.py
+++ labot/mitm/launch.py
@@ -2,14 +2,26 @@
 
 from ..logs import logger
 from .proxy import startProxyServer
-from .bridge import PrintingMsgBridgeHandler
+from .bridge import *
 from .proxychains import launchDofus
 
 
 logger.setLevel("INFO")
 
 # to interrupt : httpd.shutdown()
-httpd = startProxyServer(PrintingMsgBridgeHandler.proxy_callback)
+bridges = []
+
+
+def my_callback(coJeu, coSer):
+    global bridges
+    bridge = InjectorBridgeHandler(coJeu, coSer)
+    bridges.append(bridge)
+    bridge.loop()
+
+
+httpd = startProxyServer(my_callback, debug=True)
+
+# TODO: option to launch
 
 # you can launch several instances
 # of dofus with the same httpd
