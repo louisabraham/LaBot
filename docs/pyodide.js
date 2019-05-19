22318be8b637bf5d1f79ac870e1e981684a9d8ea
louisabraham <louis.abraham@yahoo.fr>
2019-05-19 19:27:46 +0200

    test other imports

--- docs/pyodide.js
+++ docs/pyodide.js
@@ -6,7 +6,7 @@ var languagePluginLoader = new Promise((resolve, reject) => {
   // This is filled in by the Makefile to be either a local file or the
   // deployed location. TODO: This should be done in a less hacky
   // way.
-  const baseURL = 'https://iodide-project.github.io/pyodide-demo/';
+  const baseURL = './';
 
   ////////////////////////////////////////////////////////////
   // Package loading
