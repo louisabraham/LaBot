0b61859099631708b24665197d0022bcc3a9c7a6
Blizzi <40988389+kenny94xxhell@users.noreply.github.com>
2020-04-19 18:11:29 +0200

    Add windows path to --launch Dofus.exe

--- scripts/mitm.py
+++ scripts/mitm.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
+import os
 import argparse
-import sys
 from pathlib import Path
 from subprocess import Popen
 
@@ -15,11 +15,13 @@ from labot.mitm.bridge import *
 from fritm import hook, start_proxy_server
 import frida
 
+appdata = os.getenv("appdata")
+parent = os.path.dirname(appdata)
 
 DOFUS_PATH = {
     "darwin": "/Applications/Dofus.app/Contents/Data/Dofus.app/Contents/MacOS/Dofus",
     "linux": None,
-    "win32": None,
+    "win32": str(parent) + "\Local\Ankama\zaap\dofus\Dofus.exe",
     "cygwin": None,
 }
 
